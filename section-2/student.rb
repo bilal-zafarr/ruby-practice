f097a831087f2d9db393bd5913361c3dbe54c667
Bilal Zafar <bilalzaafar@gmail.com>
2022-02-16 15:33:20 +0500

    sync

---
+++ section-2/student.rb
@@ -0,0 +1,16 @@
+class Student
+
+  attr_accessor :name, :grade
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def to_s
+    "#{@name} is in grade #{@grade}"
+  end
+  
+end
+
+bilal = Student.new("Bilal", "A")
+puts bilal
