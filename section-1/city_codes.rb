f84ae1d6d3fbb683667e3843d32983aa710d66fc
Bilal Zafar <bilalzaafar@gmail.com>
2022-02-16 14:27:32 +0500

    sync

---
+++ section-1/city_codes.rb
@@ -0,0 +1,27 @@
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+ 
+# Get city names from the hash
+def get_city_names(somehash)
+  somehash.keys
+end
+ 
+# Get area code based on given hash and key
+def get_area_code(somehash, key)
+  somehash[key]
+end
+ 
+# Execution flow
+loop do
+  
+end
