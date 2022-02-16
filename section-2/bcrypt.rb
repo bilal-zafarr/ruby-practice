83e09dd1c114f008f6a02fc0aee532e1d36be270
Bilal Zafar <bilalzaafar@gmail.com>
2022-02-16 22:28:00 +0500

    sync

--- section-2/bcrypt.rb
+++ section-2/bcrypt.rb
@@ -11,4 +11,3 @@ puts my_password == "not my password" #=> false
 # my_password = BCrypt::Password.new("$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa")
 # my_password == "my password"     #=> true
 # my_password == "not my password" #=> false
-#
